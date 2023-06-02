75e093d01dfa23d6fa8cbe5e55f6ef3b22dae315
Vishnureddy123456 <vishnureddylekkala@gmail.com>
2023-06-02 12:58:51 +0530

    first commit

---
+++ india/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sehwag(request):
+    return render(request,'sehwag.html')
--- project11/settings.py
+++ project11/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rcb',
     'csk',
+    'india',
 ]
 
 MIDDLEWARE = [
